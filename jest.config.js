935882ee37f3bde1c431248614d389b0365e9023
Chris Mc <prince.chrismc@gmail.com>
2021-02-25 21:07:30 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: ['**/*.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
